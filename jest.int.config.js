4b9ad2c4c86e799db692e0fea6c86b2acc8099bc
Deepali Chavan <deepali.chavan@broadcom.com>
2020-10-21 15:46:15 +0200

    Jest Setup

---
+++ jest.int.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    testEnvironment: 'node', // make test faster
+
+    // ts preprocessor
+testMatch: ['**/__tests__/sampletests/**/*.test.ts'],
+    preset: 'ts-jest',
+
+    // coverage
+    coverageDirectory: '<rootDir>/results/unit/coverage',
+    coverageReporters: ['text', 'text-summary', 'lcov'],
+
+    // reporters
+    reporters: [
+      'default',
+      [
+        './node_modules/jest-html-reporter',
+        {
+          pageTitle: 'CHE4Z Explorer for Endevor Unit Test Report',
+          outputPath: 'results/unit/results.html',
+        },
+      ],
+    ],
+  };
